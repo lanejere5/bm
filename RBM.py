a15810fcb5e39949281bcbfade4a37b0916f47f0
Jeremy Lane <lane203j@gmail.com>
2018-05-25 15:21:36 +0200

    fixed RBM bugs

--- RBM.py
+++ RBM.py
@@ -2,6 +2,8 @@
 '''
     A class defining a restricted Boltzmann machine.
 
+
+
 '''
 import numpy as np
 import random
@@ -9,14 +11,18 @@ import matplotlib.pyplot as plt
 
 learning_rate = 0.01
 
-def sigmoid(x):
-    return np.divide(1., 1.+np.exp(-x))
+def sigmoid(x): # overflow protected sigmoid
+    return 1/(1+np.exp(-x))
 
 class RBM:
 
     kind = 'RBM'
 
     def __init__(self,v_dim,h_dim):
+        '''
+            v_dim = dimension of the visible layer
+            h_dim = dimension of the hidden layer
+        '''
         self.v_dim = v_dim
         self.h_dim = h_dim
         self.W = np.zeros((self.v_dim,self.h_dim))
@@ -30,8 +36,8 @@ class RBM:
             Initialize with trained weights.
         '''
         W,a,b = weights['W'],weights['a'],weights['b']
-        assert (np.shape(W)[0] == np.shape(a)[0]) and (np.shape(W)[1] == np.shape(b)[0])
-        rbm = cls(np.shape(W)[0],np.shape(W)[1])
+        assert (W.shape[0] == a.shape[0]) and (W.shape[1] == b.shape[0])
+        rbm = cls(W.shape[0],W.shape[1])
         rbm.W = W
         rbm.a = a
         rbm.b = b
@@ -51,7 +57,7 @@ class RBM:
             - a has shape (v_dim,1)
             - W has shape (v_dim,h_dim)
         '''
-        #assert(np.shape(h)[0] == self.h_dim)
+        assert(h.shape[0] == self.h_dim)
         v_probs = sigmoid(self.a + np.dot(self.W,h))
         assert(not np.sum(np.isnan(v_probs)))
         return np.random.binomial(1,v_probs)
@@ -63,85 +69,103 @@ class RBM:
             - b has shape (h_dim,1)
             - W has shape (v_dim,h_dim)
         '''
-        #assert(np.shape(v)[0] == self.v_dim)
-        h_probs = sigmoid(self.b + np.dot(np.transpose(self.W),v))
+        assert(v.shape[0] == self.v_dim)
+        h_probs = sigmoid(self.b + np.dot(self.W.T,v))
         assert(not np.sum(np.isnan(h_probs)))
-        return np.random.binomial(1,h_probs) # when n=1, binomial = bernoulli
+        return np.random.binomial(1,h_probs)
 
-    def gradient_update(self,v,learning_rate):
-        '''
-            Input:
-            - v has shape (v_dim,m)
-            - h has shape (h_dim,m)
-            - a has shape (v_dim,1)
-            - b has shape (h_dim,1)
-            - W has shape (v_dim,h_dim)
-        '''
-
-        #assert(np.shape(v)[0] == self.v_dim)
-        # sample distributions
-        h       = self.generate_h_sample(v)
-        v_prime = self.generate_v_sample(h)
-        h_prime = self.generate_h_sample(v_prime)
-        
-        # compute the gradients, averaged over mini-batch
-        DW = -np.mean(v[:,np.newaxis,:]*h[np.newaxis,:,:] - v_prime[:,np.newaxis,:]*h_prime[np.newaxis,:,:], axis = 2)
-        Da = -np.mean(v - v_prime, axis = 1,keepdims = True).reshape((self.v_dim,1))
-        Db = -np.mean(h - h_prime, axis = 1,keepdims = True).reshape((self.h_dim,1))
-        
-        # update weights and biases
-        self.W = self.W + learning_rate*DW
-        self.a = self.a + learning_rate*Da
-        self.b = self.b + learning_rate*Db
-        
-        return
-
-    def train(self, x, epochs = 10, batch_size = 16, learning_rate = learning_rate, plot = False):
+    def train(self, x, epochs = 10, batch_size = 20, learning_rate = learning_rate, plot = False):
         ''' 
             Input:
             - x has shape (v_dim, number_of_examples)
 
         '''
-        #assert(np.shape(x)[0]==self.v_dim)
+        assert(x.shape[0]==self.v_dim)
+
+        np.random.seed(0)
+
+        # hyperparameters used by Hinton et al. for MNIST
+        initialmomentum  = 0.5
+        finalmomentum    = 0.9
+        weightcost       = 0.0002
+        num_minibatches  = int(x.shape[1]/batch_size)
+
+        DW = np.zeros((self.v_dim,self.h_dim))
+        Da = np.zeros((self.v_dim,1))
+        Db = np.zeros((self.h_dim,1))
+
         # initialize weights and parameters
-        self.W = np.random.normal(0.,0.01,size = (self.v_dim,self.h_dim))
+        self.W = np.random.normal(0.,0.1,size = (self.v_dim,self.h_dim))
         # visible bias a_i is initialized to ln(p_i/(1-p_i)), p_i = (proportion of examples where x_i = 1)
-        self.a = (np.log(np.mean(x,axis = 1,keepdims=True)+1e-10) - np.log(1-np.mean(x,axis = 1,keepdims=True)+1e-10)) 
+        #self.a = (np.log(np.mean(x,axis = 1,keepdims=True)+1e-10) - np.log(1-np.mean(x,axis = 1,keepdims=True)+1e-10))
+        self.a = np.zeros((self.v_dim,1))
         self.b = np.zeros((self.h_dim,1))
-        
-        num_minibatches = int(np.shape(x)[0]/batch_size)
-        
+
+        #print(self.W[0,0],self.a[0,0],self.b[0,0])
+
         for i in range(epochs):
             print("Epoch %i"%(i+1))
-            np.random.shuffle(x)
+            np.random.shuffle(x.T)
+
+            if i>5:
+                momentum = finalmomentum
+            else: 
+                momentum = initialmomentum
             
             for j in range(num_minibatches):
-                v = x[:,i:i+batch_size]
-                self.gradient_update(v,learning_rate)
+                v = x[:,j*batch_size:(j+1)*batch_size]
+
+                # sample distributions
+                h     = self.generate_h_sample(v)
+                v_neg = self.generate_v_sample(h)
+                h_neg = self.generate_h_sample(v_neg)
+                
+                # compute the gradients, averaged over minibatch, with momentum and regularization
+                DW = momentum*DW + learning_rate*(np.mean(v[:,np.newaxis,:]*h[np.newaxis,:,:] - v_neg[:,np.newaxis,:]*h_neg[np.newaxis,:,:], axis = 2)-weightcost*self.W)
+                Da = momentum*Da + learning_rate*np.mean(v - v_neg, axis = 1,keepdims = True) #.reshape((self.v_dim,1))
+                Db = momentum*Db + learning_rate*np.mean(h - h_neg, axis = 1,keepdims = True) #.reshape((self.h_dim,1))
+                
+                # update weights and biases
+                self.W = self.W + DW
+                self.a = self.a + Da
+                self.b = self.b + Db
 
             if plot == True:
+                self.plot_weight_histogram()
                 self.plot_weights()
-            
-        
+
+                v,_ = self.gibbs_sampling(1,1)
+                plt.imshow(v.reshape((28,28)),cmap=plt.cm.gray)
+                plt.show()
+
+                v,_ = self.gibbs_sampling(1,1,x[:,0].reshape((self.v_dim,1)))
+                plt.imshow(v.reshape((28,28)),cmap=plt.cm.gray)
+                plt.show()
+
         return
 
-    def gibbs_sampling(self, n=1, m=1):
+    def gibbs_sampling(self, n=1, m=1,v=None):
         '''
             n - number of iterations of blocked Gibbs sampling
         '''
-        v_probs = np.full((self.v_dim,m),0.5)
-        v = np.random.binomial(1,v_probs)
-        #print(np.shape(v))
-        
+        if v is None:
+            v_probs = np.full((self.v_dim,m),0.5)
+            v = np.random.binomial(1,v_probs)
+
         h = self.generate_h_sample(v)
-        
         for i in range(n):
             v = self.generate_v_sample(h)
             h = self.generate_h_sample(v)
-        
         return v,h
 
     def plot_weights(self):
+        '''
+            For debugging 
+        '''
+        
+        return
+
+    def plot_weight_histogram(self):
         '''
             For debugging 
         '''
@@ -162,6 +186,7 @@ class RBM:
         plt.tight_layout()
 
         plt.show()
+        return
 
     def save(self, filename):
         '''
@@ -187,8 +212,8 @@ class RBM:
             Save RBM weights to file
         '''
         W = np.loadtxt(filename + '_W.csv',delimiter=",")
-        a = np.loadtxt(filename + '_a.csv',delimiter=",").reshape((np.shape(W)[0],1))
-        b = np.loadtxt(filename + '_b.csv',delimiter=",").reshape((np.shape(W)[1],1))
+        a = np.loadtxt(filename + '_a.csv',delimiter=",").reshape((W.shape[0],1))
+        b = np.loadtxt(filename + '_b.csv',delimiter=",").reshape((W.shape[1],1))
         return {"W":W,"a":a,"b":b}
 
 
