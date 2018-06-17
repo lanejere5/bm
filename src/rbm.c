824480d018722f2c0fe8add56f3cba4ab4d522cb
James Mracek <james.mracek@adroll.com>
2018-06-17 01:11:48 -0600

    SIMD matrix-vector multiplication for 8x8 matrices

--- src/rbm.c
+++ src/rbm.c
@@ -33,8 +33,9 @@ typedef struct RBM {
     uint32_t r, c; // 8 bytes. Smallest multiples of 8 greater than v_dim and h_dim
     float8** W; // Store the rows of W
     float8** WT; // Also store the columns of W
-};
+} RBM;
 
+/*
 //FIXME: Update for addition of WT
 RBM* init_rbm(unsigned int v, unsigned int h) {
     
@@ -75,7 +76,7 @@ void destroy_rbm(RBM* r) {
     // Finally free the memory being used to store the RBM
     free(r);
 }
-
+*/ 
 /*************************************************************/
 
 //  Utilities for matrix-vector multiplication using SIMD and multithreaded processing
@@ -83,34 +84,83 @@ void destroy_rbm(RBM* r) {
 /*************************************************************/
 
 // Helper struct for distributed heap allocated memory to multiple threads without having to create a lot of copies
-typedef struct Av_add_b {
+typedef struct LinearMapData {
     // This packs nicely into a 32-byte struct
-    float8** A;      // 8 bytes
-    float8* v;       // 8 bytes
-    float8* b;       // 8 bytes
-    float8* result;  // 8 bytes
-    uint32_t l;     // 4 bytes
-};
+    float8** A;      // 8 bytes The matrix
+    float8* v;       // 8 bytes The vector to multiply
+    float8* result;  // 8 bytes Place to store the answer
+    uint32_t l;      // 4 bytes Number of columns of A = len(v)
+} LinearMapData;
 
+// Helper struct for passing multiple arguments to a threaded function
 typedef struct ThreadArgs {
-    Av_add_b* data;
+    LinearMapData* data;
     uint32_t threadID;
-};
+} ThreadArgs;
 
-void multiply_block(float8* rows, float8 col, float8* out) {
-     float temp[8];
-     temp[0] = _mm256_mul_ps(rows[0],col);
+void multiply_block(float8* rows, float8* col, float8* out) {
+    /*  This picture is helping me understand right now. I'm tired.
+
+    x * * * * * * * *  *
+    y * * * * * * * *  *
+    u * * * * * * * *  *
+    v * * * * * * * *  *
+    a * * * * * * * *  *
+    b * * * * * * * *  *
+    c * * * * * * * *  *
+    d * * * * * * * *  *
+
+    What is hadd doing:
+    x1 x2 x3 x4 x5 x6 x7 x8 : y1 y2 y3 y4 y5 y6 y7 y8
+    hadd again...
+    x12 x34 y12 y34 x56 x78 y56 y78 
+    a12 a34 b12 b34 a56 a78 b56 b78
+
+    Gives this.
+    x1234 y1234 a1234 b1234 x5678 y5678 a5678 b5678
+
+    Then permute the bits to get this:
+    x1234 x5678 y1234 y5678 a1234 a5678 b1234 b5678
+
+    Meanwhile, do the same thing to the other rows:
+    u1234 u5678 v1234 v5678 c1234 c5678 d1234 d5678
+
+    A final hadd gives:
+
+    x y u v a b c d
+
+    Which is what we want.
+
+    */
+     
+    float8 buf[8];
+
+    // x1234 y1234 a1234 b1234 x5678 y5678 a5678 b5678
+    //   0     4     1     5     2     6     3     7 
+    // Implement the permutation above
+    const float8 shuffle = _mm256_set_epi32(7,3,6,2,5,1,4,0);
+
+    // Multiply rows of A against the column vector
+    buf[0] = _mm256_mul_ps(rows[0],*col);
+    buf[1] = _mm256_mul_ps(rows[1],*col);
+    buf[2] = _mm256_mul_ps(rows[2],*col);
+    buf[3] = _mm256_mul_ps(rows[3],*col);
+    buf[4] = _mm256_mul_ps(rows[4],*col);
+    buf[5] = _mm256_mul_ps(rows[5],*col);
+    buf[6] = _mm256_mul_ps(rows[6],*col);
+    buf[7] = _mm256_mul_ps(rows[7],*col);
+
+    *out = _mm256_add_ps(*out, _mm256_hadd_ps(
+                                    _mm256_permutevar8x32_ps(_mm256_hadd_ps(_mm256_hadd_ps(buf[0],buf[1]), _mm256_hadd_ps(buf[4],buf[5])), shuffle),
+                                    _mm256_permutevar8x32_ps(_mm256_hadd_ps(_mm256_hadd_ps(buf[2],buf[3]), _mm256_hadd_ps(buf[6],buf[7])), shuffle)
+                                ));
 }
-// Block 11 * y1 + Block12 * y2 + ...
-/*  This picture is helping me understand right now. I'm tired.
 
-* * * *  *
-* * * *  *
-* * * *  *
-* * * *  *
+/*
+
+We parallelize matrix-vector multiplication by using block_muliply to compute 8 values of the outupt vector in each thread - i.e. we break the matrix A up into 8 row strips, and assign each strip it's own thread.
 
 */
-
 
 // This is the function that dispatches work to all the threads.  The k'th thread will be assigned all the rows j such that floor( j / MAX_THREADS ) = k
 void affine(Av_add_b* data) {
@@ -179,6 +229,8 @@ void delete_rbm(RBM* r) {
 
 }
 
+*/
+
 // Intrinsic exponential function
 float8 _mm256_exp_ps(float8 x) {
     // Evaluates exp(x) using Taylor series + Horner's method
@@ -215,18 +267,27 @@ float* sigmoid(float* x, unsigned int N) {
 
 int main(void) {
 
-    // Initialize a vector of ones
+    // Initialize a vector of ones for sigmoid test
     float __attribute__ (( aligned(32) )) ones[2048] = {[0 ... 2047] = 1.0};
     clock_t start, end;
     float* res = (float *) _mm_malloc(2048*sizeof(float),32);
+    
+    // Initialize an 8x8 block of 1s
+    float8 rows[8];
+    for (int i = 0; i < 8; i++) rows[i] = _mm256_set_ps(1,2,3,4,5,6,7,8);
+    float8 col = _mm256_set_ps(7.0, 6.0, 5.0, 0.0, 0.0, 4.0, 0.0, 1.0);
+    float8 out = _mm256_set1_ps(0.0); 
 
     start = clock();
-    for (int i = 0; i < 10000; i++) {
-        res = sigmoid(ones, 2048); 
+    for (int i = 0; i < 1000000; i++) {
+        out = _mm256_set1_ps(0.0); 
+        multiply_block(rows,&col,&out); 
     }
     end = clock();
-    printf("%f %f %f %f", res[0],res[100],res[245],res[1890]);
-    printf("%lu nanoseconds\n", ((end - start) / (CLOCKS_PER_SEC / 100000 )));
+    
+    float* res2 = (float *) &out;
+    printf("%f %f %f %f %f %f %f %f \n", res2[0],res2[1],res2[2],res2[3],res2[4], res2[5], res2[6], res2[7]);
+    printf("%lu nanoseconds\n", (1000000000 * (end - start) / (CLOCKS_PER_SEC) ));
    
     _mm_free(res);
     return 0;
